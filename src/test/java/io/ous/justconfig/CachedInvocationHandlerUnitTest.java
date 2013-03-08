599add1804317ba838816d5652ceb0221c7d43c6
Asafh <asafh.gh@ous.im>
2013-03-08 20:01:44 +0200

    Removing duplicate test

--- src/test/java/io/ous/justconfig/CachedInvocationHandlerUnitTest.java
+++ src/test/java/io/ous/justconfig/CachedInvocationHandlerUnitTest.java
@@ -115,41 +115,4 @@ public class CachedInvocationHandlerUnitTest {
 			throw new IllegalStateException(e);
 		}
 	}
-	
-	@Test
-	public void testCachexd() throws SecurityException, Throwable {
-		ExecutionTargetRunner runner = new ExecutionTargetRunner(new ScheduledExecutionStrategy(50, 100, 50));
-		runner.run(new ExecutionTarget<Void>() {
-		protected CachedInvocationHandler cachedInvocationHandler;
-		protected InvocationHandler wrapped;
-		protected Object unique;
-			@Override
-	  	public void before() throws Throwable {
-				wrapped = mock(InvocationHandler.class);
-				cachedInvocationHandler = new CachedInvocationHandler(wrapped);
-				unique = new Object();
-				when(wrapped.invoke(PROXY, x, NO_ARGS)).thenReturn(unique);
-			}
-	    
-	    @Override
-			public Void call() throws Exception {
-				try {
-					assertEquals(unique, cachedInvocationHandler.invoke(PROXY, x, NO_ARGS));
-					assertEquals(unique,cachedInvocationHandler.invoke(PROXY, x, NO_ARGS));
-					assertEquals(unique,cachedInvocationHandler.invoke(PROXY, x, NO_ARGS));
-					assertEquals(unique,cachedInvocationHandler.invoke(PROXY, x, NO_ARGS));
-					
-					return null;
-				}
-				catch (Throwable e) {
-					return Throwables.asException(e);
-				}
-			}
-			@Override
-			public void after() throws Throwable {
-				verify(wrapped, times(1)).invoke(PROXY, x, NO_ARGS);
-				verifyNoMoreInteractions(wrapped);
-			}
-		});
-	}
 }
